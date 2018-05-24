fa49b421a4226436ca7912f946e7ab917852c94c
ArisNunez <arisnunez@gmail.com>
2018-05-24 19:03:54 -0400

    Mobile styles and scripts

--- public/static/js/site.js
+++ public/static/js/site.js
@@ -1,5 +1,5 @@
 var responsive_window = function(){
-	var max_width = 800;
+	var max_width = 1200;
 	$(window).resize(function(){
 		if($(window).width() <= max_width){
 
@@ -71,13 +71,9 @@ $("#lightbox_contain").click(function(e){
 	}
 })
 
-
 // Home Theater
 var home_theater = function(){	
 	var scroll_ready = 0;
-
-	//Initial Slide
-	$(".theater_list >li:first").addClass("active");
 
 	//Slide Index
 	var slide_amnt = $(".theater_list > li").length;
@@ -95,60 +91,116 @@ var home_theater = function(){
 
 	//Slide Scroll
 	var scroll_through = false;
-	$('#home_theater').bind('mousewheel DOMMouseScroll', function(e){
+	$('#home_theater').bind('mousewheel DOMMouseScroll', theater_slide_action);
+
+
+	// Touch Slide
+	$("#home_theater").bind('touchstart', handleTouchStart);        
+	$("#home_theater").bind('touchmove', handleTouchMove);
+
+	var xDown = null;
+	var yDown = null;
+
+	function handleTouchStart(e){
+	   xDown = e.touches[0].clientX;
+	   yDown = e.touches[0].clientY;
+	};
+
+	function handleTouchMove(e){
+		if(scroll_through == false){
+			e.preventDefault();
+		}
+
+		if(! xDown || ! yDown){return;}
+
+		var xUp = e.touches[0].clientX;
+		var yUp = e.touches[0].clientY;
+
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+
+	   if(Math.abs( xDiff )+Math.abs( yDiff )>150){ //to deal with to short swipes
+
+	    	if(Math.abs(xDiff) > Math.abs(yDiff)){/*most significant*/
+	    		           
+	    	}else{
+	    		if(yDiff > 0){/* up swipe */
+	    			scroll_down();
+	    		}else{ /* down swipe */
+	    			scroll_up();
+	    		}
+	    	}
+
+	    	/* reset values */
+	    	xDown = null;
+	    	yDown = null;
+	   }
+	};
+
+	function theater_slide_action(e){
 		if(scroll_through == false){
 			e.preventDefault();
 		}
 	 	if(scroll_ready == 0){	
 		   if(e.originalEvent.detail < 0  || e.originalEvent.wheelDelta > 0) { //Scrolling Up
-		   	if($(".theater_list >li.active").prev("li").length != 0){
-		      	$(".theater_list >li.active").prev().addClass("active");
-		      	$(".theater_list >li.active:last").removeClass("active");
-		      	$(".theater_list >li.up:last").removeClass("up");
-
-		      	//Slide Index
-		      	$(".slide_index >li.active").prev().addClass("active");
-		      	$(".slide_index >li.active").last().removeClass("active");
-
-		      	//Action Text
-		      	var action_text = $(".theater_list >li.active:last .action_text").html();
-		      	$(".slide_action span").html(action_text);
-
-		      	//Scroll Actions
-		      	if(scroll_through == 1){
-		      		scroll_through = 0;
-		      		var body = $("html, body");
-		      		body.stop().animate({scrollTop: 0}, 500, 'swing');
-		      	}
-		      }
+		   	scroll_up()
 		   }
 		   else{ //Scrolling Down
-		   	if($(".theater_list >li.active:last").next("li").length != 0){
-			      $(".theater_list >li.active").addClass("up");
-			      $(".theater_list >li.active").next().addClass("active");
-
-			      //Slide Index
-		      	$(".slide_index >li.active").next().addClass("active");
-		      	$(".slide_index >li.active").first().removeClass("active");
-
-		      	//Action Text
-		      	var action_text = $(".theater_list >li.active:last .action_text").html();
-		      	$(".slide_action span").html(action_text);
-
-		      	//Scroll Actions
-		      	if($(".theater_list > li").last().hasClass("active")){
-		      		setTimeout(release_scroll, 1000);
-		      		function release_scroll(){
-		      			scroll_through = true;
-		      		}		      		
-		      	}
-			   }
+		   	scroll_down()
 		   }
+
 		   scroll_ready = 1;
 		   setTimeout(release_scroll, 1000);
 		   function release_scroll(){
 		   	scroll_ready = 0;
 		   }
 	 	}
-	});	
+	}
+
+   function scroll_up(){
+		if($(".theater_list >li.active").prev("li").length != 0){
+	   	$(".theater_list >li.active").prev().addClass("active");
+	   	$(".theater_list >li.active:last").removeClass("active");
+	   	$(".theater_list >li.up:last").removeClass("up");
+
+	   	//Slide Index
+	   	$(".slide_index >li.active").prev().addClass("active");
+	   	$(".slide_index >li.active").last().removeClass("active");
+
+	   	//Action Text
+	   	var action_text = $(".theater_list >li.active:last .action_text").html();
+	   	$(".slide_action span").html(action_text);
+
+	   	//Scroll Actions
+	   	if(scroll_through == 1){
+	   		scroll_through = 0;
+	   		var body = $("html, body");
+	   		body.stop().animate({scrollTop: 0}, 500, 'swing');
+	   	}
+	   }
+   }
+
+   function scroll_down(){   	
+   	if($(".theater_list >li.active:last").next("li").length != 0){
+	      $(".theater_list >li.active").addClass("up");
+	      $(".theater_list >li.active").next().addClass("active");
+
+	      //Slide Index
+      	$(".slide_index >li.active").next().addClass("active");
+      	$(".slide_index >li.active").first().removeClass("active");
+
+      	//Action Text
+      	var action_text = $(".theater_list >li.active:last .action_text").html();
+      	$(".slide_action span").html(action_text);
+
+      	//Scroll Actions
+      	if($(".theater_list > li").last().hasClass("active")){
+      		setTimeout(release_scroll, 1000);
+      		function release_scroll(){
+      			scroll_through = true;
+      		}		      		
+      	}
+	   }
+   }
+	
 }();
